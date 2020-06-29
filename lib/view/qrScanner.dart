b49c2277b63b2b3e5e9d424e696e25bd48fe376d
gene-sbay <geneb.contact@gmail.com>
2020-06-29 14:30:29 -0700

    QrScannerScreen now looking stable with working Close Scanner button

--- lib/view/qrScanner.dart
+++ lib/view/qrScanner.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:qr_mobile_vision/qr_camera.dart';
 import 'package:logger/logger.dart';
 import 'package:qr_code_demo/app/logger.dart';
+import 'package:qr_mobile_vision/qr_camera.dart';
 
 void main() => runApp(QrScannerScreen());
 
 class QrScannerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter QR Scanner',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: QrScannerStatefulWidget(),
-    );
+    return new QrScannerStatefulWidget();
   }
 }
 
@@ -25,25 +18,132 @@ class QrScannerStatefulWidget extends StatefulWidget {
 }
 
 class _QrScannerState extends State<QrScannerStatefulWidget> {
-  String _qrInfo = 'Scan a Code';
-  bool _camState = false;
+  static const DEFAULT_SCAN_VALUE = 'Scan a QR Code';
+
+  String _qrCodeData;
+  bool _isCameraOn = false;
+
+  List<Widget> _mainColumnWidgets;
 
   Logger log = getLogger("QrScannerScreen");
 
   _qrCallback(String code) {
     setState(() {
-      _camState = false;
-      _qrInfo = code;
+      _isCameraOn = false;
+      _qrCodeData = code;
 
-      log.i('_qrInfo: $_qrInfo');
+      log.i('_qrCodeData: $_qrCodeData');
     });
   }
 
   _scanCode() {
     setState(() {
-      _camState = true;
+      _isCameraOn = true;
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    _mainColumnWidgets = [];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code Scanner'),
+      ),
+      // body: Text(_qrCodeData),
+      body: Container(
+          // color: Colors.amber,
+          child: Center(
+        child: _getMainLayout(),
+      )),
+      floatingActionButton: Visibility(
+        visible: !_isCameraOn,
+        child: FloatingActionButton(
+          onPressed: _scanCode,
+          tooltip: 'Scan',
+          child: Icon(Icons.scanner),
+        ),
+      ),
+    );
+  }
+
+  _clearScannedData() {
+    _qrCodeData = null;
+  }
+
+  Widget _getMainLayout() {
+    if (_isCameraOn) {
+      _setActiveCameraView();
+    } else {
+      _setCameraOffView();
+    }
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: _mainColumnWidgets,
+    );
+  }
+
+  _setCameraOffView() {
+    if (_qrCodeData != null) {
+      _mainColumnWidgets.add(
+        Text(
+          'QR Code Value:',
+          style: TextStyle(fontSize: 20),
+        ),
+      );
+    } else {
+      _qrCodeData = DEFAULT_SCAN_VALUE;
+    }
+
+    _mainColumnWidgets.add(
+      Center(
+          child: Text(
+        _qrCodeData,
+        style: TextStyle(fontSize: 25),
+      )),
+    );
+  }
+
+  _setActiveCameraView() {
+    Widget closeCameraWidget = RaisedButton(
+      color: Colors.white,
+      child: Text(
+        'Close Scanner',
+        style: TextStyle(color: Colors.black, fontSize: 25),
+      ),
+      onPressed: () {
+        log.i('pressed Close Scanner');
+        setState(() {
+          _clearScannedData();
+          _isCameraOn = false;
+        });
+      },
+    );
+
+    Widget cameraWidget = Center(
+      child: SizedBox(
+        width: 512,
+        height: 256,
+        child: QrCamera(
+          onError: (context, error) => Text(
+            error.toString(),
+            style: TextStyle(color: Colors.red),
+          ),
+          qrCodeCallback: (code) {
+            _qrCallback(code);
+          },
+        ),
+      ),
+    );
+
+    _mainColumnWidgets.add(closeCameraWidget);
+    _mainColumnWidgets.add(cameraWidget);
+  }
+}
+
+/*
 
   Future<String> callServer1() async {
     // String localhost = "69.181.92.143";
@@ -61,52 +161,4 @@ class _QrScannerState extends State<QrScannerStatefulWidget> {
       throw Exception('Failed to load end point');
     }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    // callServer1();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter QR Reader'),
-      ),
-      // body: Text(_qrInfo),
-      body: _getMainLayout(),
-      floatingActionButton: Visibility(
-        visible: !_camState,
-        child: FloatingActionButton(
-          onPressed: _scanCode,
-          tooltip: 'Scan',
-          child: Icon(Icons.scanner),
-        ),
-      ),
-    );
-  }
-
-  Widget _getMainLayout() {
-    Widget cameraWidget = _camState
-        ? Center(
-            child: SizedBox(
-              width: 512,
-              height: 256,
-              child: QrCamera(
-                onError: (context, error) => Text(
-                  error.toString(),
-                  style: TextStyle(color: Colors.red),
-                ),
-                qrCodeCallback: (code) {
-                  _qrCallback(code);
-                },
-              ),
-            ),
-          )
-        : Center(child: Text(_qrInfo));
-
-    return Column(
-      children: [
-        cameraWidget
-      ]
-    );
-  }
-}
-
+ */
